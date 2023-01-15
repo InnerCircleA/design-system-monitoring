8cfac5cd2267699af30fde36f62cdabd314b16de
손지호 <sonjiho@sonjihoui-MacBookPro.local>
2023-01-15 21:51:14 +0900

    Fix bug parsing component properties

--- packages/component-tracking-plugin/src/ast/react-element-expression.js
+++ packages/component-tracking-plugin/src/ast/react-element-expression.js
@@ -1,67 +1,77 @@
-
-const estraverse = require("estraverse");
-const escodegen = require("escodegen");
+const estraverse = require('estraverse');
+const escodegen = require('escodegen');
 /**
- * AST Expression 이 React Element를 생성하는 함수 호출인지 확인해주는 Util 함수 
+ * AST Expression 이 React Element를 생성하는 함수 호출인지 확인해주는 Util 함수
  * @param {*} expression 검증할 AST Expression 객체
- * @returns {boolean} 
+ * @returns {boolean}
  */
-const checkJSXCallExpression = (expression) => expression.type === "CallExpression"
-    && expression.callee?.type === "Identifier"
-    && expression.callee.name === "_jsx";
+const checkJSXCallExpression = (expression) =>
+  expression.type === 'CallExpression' &&
+  expression.callee?.type === 'Identifier' &&
+  expression.callee.name === '_jsx';
 
 /**
  * AST 분석을 통해 React Element를 생성하는 함수 CallExpression을 모두 찾아서 호출 정보 반환
- * @param {*} ast 
- * @returns 
+ * @param {*} ast
+ * @returns
  */
 function getReactComponentsFromAST(ast) {
-    const result = [];
+  const result = [];
 
-    estraverse.traverse(ast, {
-        enter: function (node, parent) {
-            if (!checkJSXCallExpression(node)) return;
+  estraverse.traverse(ast, {
+    enter: function (node, parent) {
+      if (!checkJSXCallExpression(node)) return;
 
-            const jsxCallExpression = node;
-            const args = jsxCallExpression.arguments;
-            if (args === undefined || args.length === 0) return;
+      const jsxCallExpression = node;
+      const args = jsxCallExpression.arguments;
+      if (args === undefined || args.length === 0) return;
 
-            const elementType = args[0];
-            const propsObjExpression = args[1];
+      const elementType = args[0];
+      const propsObjExpression = args[1];
 
-            // when type is "Identifier", React Component 
-            if (elementType.type !== "Identifier") return;
+      // when type is "Identifier", React Component
+      if (elementType.type !== 'Identifier') return;
 
-            const componentName = elementType.name;
-            const componentProps = {};
+      const componentName = elementType.name;
+      const componentProps = {};
+      let spread = false;
 
-            if (args.length > 1) {
-                for (const property of propsObjExpression.properties) {
-                    const key = property.key.name;
-                    let value = undefined;
+      if (args.length > 1) {
+        for (const property of propsObjExpression.properties) {
+          if (property.type === 'Property') {
+            const key = property.key.name;
+            let value = undefined;
 
-                    if (property.value.type === "Literal") {
-                        value = property.value.value;
-                    } else if (property.value.type === "Identifier" || property.value.type === "ObjectExpression") {
-                        value = {
-                            type: property.value.type,
-                            value: escodegen.generate(property.value) // TODO: Need to replace 
-                        }
-                    }
-                    componentProps[key] = value;
-                }
+            if (property.value.type === 'Literal') {
+              value = property.value.value;
+            } else if (
+              property.value.type === 'Identifier' ||
+              property.value.type === 'ObjectExpression'
+            ) {
+              value = {
+                type: property.value.type,
+                value: escodegen.generate(property.value),
+              };
             }
-
-            result.push({
-                name: componentName,
-                props: componentProps
-            })
+            componentProps[key] = value;
+          } else if (property.type === 'SpreadElement') {
+            // when use spread assign
+            if (!spread) spread = true;
+          }
         }
-    })
+      }
 
-    return result;
+      result.push({
+        name: componentName,
+        props: componentProps,
+        spread,
+      });
+    },
+  });
+
+  return result;
 }
 
 module.exports = {
-    getReactComponentsFromAST
-}
+  getReactComponentsFromAST,
+};
--- packages/sample-web/next.config.js
+++ packages/sample-web/next.config.js
@@ -1,10 +1,19 @@
 /** @type {import('next').NextConfig} */
+
+const ComponentTrackingWebpackPlugin = require('component-tracking-plugin');
+
 const nextConfig = {
   webpack: (
     config,
     { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
   ) => {
-    // Important: return the modified config
+    if (config.mode === 'production') {
+      config.plugins.push(
+        new ComponentTrackingWebpackPlugin({
+          libraryName: 'ui-toolkit',
+        })
+      );
+    }
     return config;
   },
   reactStrictMode: true,
